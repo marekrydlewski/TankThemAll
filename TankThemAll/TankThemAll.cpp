54f45c69cabec7a5de54776f03dcd2949bca2bc5
Marek Rydlewski <marek.rydlewski@hotmail.com>
2015-08-17 23:23:29 +0200

    Add opengl dependencies && seed project

--- TankThemAll/TankThemAll.cpp
+++ TankThemAll/TankThemAll.cpp
@@ -2,10 +2,25 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <GLFW/glfw3.h>
+
+using namespace std;
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int _tmain(int argc, char **argv)
 {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(500, 500);//optional
+	glutInitWindowSize(800, 600); //optional
+	glutCreateWindow("OpenGL First Window");
+ 
+	glutMainLoop();
+ 
 	return 0;
 }
 
