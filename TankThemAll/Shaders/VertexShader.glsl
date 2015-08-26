74436ac023e11e008775326f3c27f8aa4b0a2355
Marek Rydlewski <marek.rydlewski@hotmail.com>
2015-08-26 23:41:46 +0200

    Changed VShader add drawing in main

--- TankThemAll/Shaders/VertexShader.glsl
+++ TankThemAll/Shaders/VertexShader.glsl
@@ -1,13 +1,9 @@
 #version 450 core
- 
+layout(location = 0) in vec3 in_position; 
+
 void main(void)
 {
-   const vec4 vertices[6] = vec4[6](vec4( 0.25, -0.25, 0.5, 1.0),
-                                    vec4(-0.25, -0.25, 0.5, 1.0),
-                                    vec4( 0.25, 0.25, 0.5, 1.0),
-									vec4( 0.35, 0.35, 0.55, 1.0),
-									vec4( -0.35, -0.35, -0.5, 1.0),
-									vec4( -0.35, 0.8, -0.5, 1.0));
-									
-   gl_Position = vertices[gl_VertexID];
+
+gl_Position = vec4(in_position, 1.0);
+
 }
--- TankThemAll/TankThemAll.cpp
+++ TankThemAll/TankThemAll.cpp
@@ -7,27 +7,35 @@
 
 using namespace std;
 
+GameModels* gameModels;
 GLuint program;
-
 
 void renderScene(void)
 {
+
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(1.0, 0.0, 0.0, 1.0);//clear red
+	glClearColor(0.0, 0.3, 0.3, 1.0);
 
-	//use the created program
+	glBindVertexArray(gameModels->GetModel("triangle1"));
 	glUseProgram(program);
-
-	//draw 3 vertices as triangles
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glutSwapBuffers();
+}
+
+void closeCallback()
+{
+
+	std::cout << "GLUT:\t Finished" << std::endl;
+	glutLeaveMainLoop();
 }
 
 void Init()
 {
 
 	glEnable(GL_DEPTH_TEST);
+
+	gameModels = new GameModels();
+	gameModels->CreateTriangleModel("triangle1");
 
 	//load and compile shaders
 	ShaderLoader shaderLoader;
@@ -36,20 +44,34 @@ void Init()
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 }
 
-int _tmain(int argc, char **argv)
+int main(int argc, char **argv)
 {
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
+	glutInitWindowPosition(500, 500);
 	glutInitWindowSize(800, 600);
-	glutCreateWindow("Drawing my first triangle");
+	glutCreateWindow("OpenGL First Window");
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+
 	glewInit();
+	if (glewIsSupported("GL_VERSION_4_5")) //lower your version if 4.5 is not supported by your video card
+	{
+		std::cout << " OpenGL Version is 4.5\n ";
+	}
+	else
+	{
+		std::cout << "OpenGL 4.5 not supported\n ";
+	}
 
 	Init();
-
 	// register callbacks
 	glutDisplayFunc(renderScene);
+	glutCloseFunc(closeCallback);
+
 	glutMainLoop();
+
+	delete gameModels;
 	glDeleteProgram(program);
 	return 0;
 }
