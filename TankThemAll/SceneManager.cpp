8688f7406b4402f5ef0b76ce7bed1549e5b28b5e
Marek Rydlewski <marek.rydlewski@hotmail.com>
2015-09-03 23:39:00 +0200

    Disable face culling

--- TankThemAll/SceneManager.cpp
+++ TankThemAll/SceneManager.cpp
@@ -7,7 +7,7 @@ Scene_Manager::Scene_Manager()
 {
 
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
+	//glEnable(GL_CULL_FACE);
 
 	view_matrix = glm::mat4(1.0f, 0.0f, 0.0f, 0.0f,
 				  		    0.0f, 1.0f, 0.0f, 0.0f,
