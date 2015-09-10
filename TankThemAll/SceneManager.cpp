91b5064234a6ca2206c9f8498aaa73f7419243ab
Marek Rydlewski <marek.rydlewski@hotmail.com>
2015-09-10 21:09:49 +0200

    Camera class && some listners stuff

--- TankThemAll/SceneManager.cpp
+++ TankThemAll/SceneManager.cpp
@@ -9,17 +9,20 @@ Scene_Manager::Scene_Manager()
 	glEnable(GL_DEPTH_TEST);
 	//glEnable(GL_CULL_FACE);
 
-	view_matrix = glm::lookAt(
+	/*view_matrix = glm::lookAt(
 		glm::vec3(4, 3, 3), // Camera is at (4,3,3), in World Space
 		glm::vec3(0, 0, 0), // and looks at the origin
 		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
 		);
+	*/
+
+	camera = new Camera(glm::vec3(1, 1, 10), glm::vec3(0, 1, 0), YAW, PITCH);
 	projection_matrix = glm::perspective(45.0f, 16.0f / 9.0f, 0.1f, 100.0f);
 }
 
 Scene_Manager::~Scene_Manager()
 {
-	
+
 }
 
 void Scene_Manager::NotifyBeginFrame()
@@ -34,10 +37,10 @@ void Scene_Manager::NotifyDisplayFrame()
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 
 	glEnable(GL_BLEND);											// added for multitexturing tutorial
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);    
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	models_manager->Draw();
-	models_manager->Draw(projection_matrix, view_matrix);
+	models_manager->Draw(projection_matrix, GetViewFromCamera());
 }
 
 void Scene_Manager::NotifyEndFrame()
@@ -46,14 +49,19 @@ void Scene_Manager::NotifyEndFrame()
 }
 
 void Scene_Manager::NotifyReshape(int width, int height,
-								int previos_width, int previous_height)
+	int previos_width, int previous_height)
 {
 
-	
-	
+
+
 }
 
 void Scene_Manager::SetModelsManager(Managers::Models_Manager* models_m)
 {
 	models_manager = models_m;
+}
+
+glm::mat4 Scene_Manager::GetViewFromCamera()
+{
+	return this->camera->GetViewMatrix();
 }
--- TankThemAll/SceneManager.h
+++ TankThemAll/SceneManager.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "IListener.h"
 #include "ModelsManager.h"
+#include "Camera.h"
+
 namespace BasicEngine
 {
 	namespace Managers
@@ -16,12 +18,12 @@ namespace BasicEngine
 			virtual void NotifyDisplayFrame();
 			virtual void NotifyEndFrame();
 			virtual void NotifyReshape(int width, int height, int previos_width, int previous_height);
-
+			virtual glm::mat4 GetViewFromCamera();
 			void SetModelsManager(Managers::Models_Manager* models_manager);
 
 		private:
 			Managers::Models_Manager* models_manager;
-
+			Rendering::Camera* camera;
 			glm::mat4 projection_matrix;
 			glm::mat4 view_matrix;
 		};
