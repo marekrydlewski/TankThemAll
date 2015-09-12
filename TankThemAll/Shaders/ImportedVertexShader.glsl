d46aca444d17de7a30ba68d77cb2d649e8d77cfc
Marek Rydlewski <marek.rydlewski@hotmail.com>
2015-09-12 12:04:19 +0200

    Solve some bugs with binding textures && disable preprocessor error && fix shaders

--- TankThemAll/Shaders/ImportedVertexShader.glsl
+++ TankThemAll/Shaders/ImportedVertexShader.glsl
@@ -4,7 +4,7 @@ layout (location = 1) in vec2 in_texture;
 //layout (location = 2) in vec3 in_normal;
 
 
-out vec2 texture;
+out vec2 tex;
 
 uniform mat4 model_matrix;
 uniform mat4 view_matrix;
@@ -13,5 +13,5 @@ uniform mat4 projection_matrix;
 void main()
 {
     gl_Position = projection_matrix * view_matrix * model_matrix * vec4(in_position, 1);
-    texture = in_texture;
+    tex = in_texture;
 }
