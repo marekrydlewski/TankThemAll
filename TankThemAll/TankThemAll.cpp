e684e748763d6ea05efbd6625dd73d0b33501c6e
Marcin Jabłoński <jablon1994@gmail.com>
2015-09-19 13:57:18 +0200

    uncommented stuff

--- TankThemAll/TankThemAll.cpp
+++ TankThemAll/TankThemAll.cpp
@@ -25,11 +25,11 @@ int main(int argc, char **argv)
 
 	engine->GetModels_Manager()->SetModel("map", terrain);
 	
-	/*Tank* tank = new Tank();
+	Tank* tank = new Tank();
 	tank->SetProgram(engine->GetShader_Manager()->GetShader("importedModelShader"));
 	tank->Create("models\\Tiger\\Tiger_I.obj");
 
-	engine->GetModels_Manager()->SetModel("tank", tank);*/
+	engine->GetModels_Manager()->SetModel("tank", tank);
 	
 	engine->Run();
 
